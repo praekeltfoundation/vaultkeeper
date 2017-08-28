e758751ed676247dfbdcd28e8f7d0354dd308bd4
Racter <paracetamolboy@gmail.com>
2017-08-28 17:00:07 +0200

    More style edits. Changed description of package in setup.py. Updated find_packages() call. Added newline to end of Travis file.

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-install_requires = ['hvac==0.2.17+git.b817da4',
+install_requires = [
+                    'hvac==0.2.17+git.b817da4',
                     'requests',
-                    ]
-dependency_links = ['https://github.com/mracter/hvac/archive/b0811f0446760125baefea6383711035104e6df1.zip'
-                    '#egg=hvac-0.2.17+git.b817da4']
+]
+
+dependency_links = [
+    'https://github.com/mracter/hvac/archive/b0811f0446760125baefea6383711035104e6df1.zip#egg=hvac-0.2.17+git.b817da4',
+]
 
 setup(
     name='vaultkeeper',
     version='0.0.1',
-    description='An agent that fetches and renews '
-                'Vault credentials for Gunicorn-on-Docker'
-                'Django applications',
+    description='An agent that works with '
+                'vault-gatekeeper-mesos to '
+                'fetch and renew Vault credentials.',
     long_description=readme,
     install_requires=install_requires,
     extras_require={
@@ -30,7 +33,7 @@ setup(
     author_email='mary@praekelt.org',
     url='https://github.com/praekeltfoundation/vaultkeeper',
     license='BSD',
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(),
     dependency_links=dependency_links
 )
 
