f770fe0f894f8c2dc444246bcfb1822cf583a8f9
Racter <paracetamolboy@gmail.com>
2017-09-01 12:04:25 +0200

    Remove unused function and import.

--- vaultkeeper/tests/test_vaultkeeper.py
+++ vaultkeeper/tests/test_vaultkeeper.py
@@ -1,5 +1,4 @@
 import responses
-import ctypes
 import os
 import signal
 
@@ -9,26 +8,6 @@ from vaultkeeper import secret
 from fake_vault import FakeVault
 from fake_gatekeeper import FakeGatekeeper
 from multiprocessing import Process
-
-
-def terminate_thread(thread):
-    """Terminates a python thread from another thread.
-
-    :param thread: a threading.Thread instance
-    """
-    if not thread.isAlive():
-        return
-
-    exc = ctypes.py_object(SystemExit)
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
-        ctypes.c_long(thread.ident), exc)
-    if res == 0:
-        raise ValueError("nonexistent thread id")
-    elif res > 1:
-        # """if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"""
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(thread.ident, None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
 
 
 def configs():
