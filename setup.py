966f55e05f0fed7b37c29a510af45232795d5890
Racter <paracetamolboy@gmail.com>
2018-02-09 13:36:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ dependency_links = [
 
 setup(
     name='vaultkeeper',
-    version='0.1.4',
+    version='0.1.5',
     description=('An agent that works with '
                  'vault-gatekeeper-mesos to '
                  'fetch and renew Vault credentials.'),
